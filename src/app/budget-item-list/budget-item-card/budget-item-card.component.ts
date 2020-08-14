b247d9341f22eaf32464f137dc28269432bf058b
jonathan <philippe1jonathan@gmail.com>
2020-08-14 23:55:20 +0200

    add delete button (design)

--- src/app/budget-item-list/budget-item-card/budget-item-card.component.ts
+++ src/app/budget-item-list/budget-item-card/budget-item-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-budget-item-card',
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BudgetItemCardComponent implements OnInit {
 
+  @Input() isIncome: Boolean = false;
+  
   constructor() { }
 
   ngOnInit(): void {
