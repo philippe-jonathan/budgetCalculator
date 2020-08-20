0c35accdc9ae43ba376f0b2f63562c7389d22f95
jonathan <philippe1jonathan@gmail.com>
2020-08-20 14:24:22 +0200

    Updated total budget and app is completed

--- src/app/budget-item-list/budget-item-list.component.ts
+++ src/app/budget-item-list/budget-item-list.component.ts
@@ -8,10 +8,12 @@ import { MatDialog } from '@angular/material/dialog';
   templateUrl: './budget-item-list.component.html',
   styleUrls: ['./budget-item-list.component.scss']
 })
+
 export class BudgetItemListComponent implements OnInit {
 
   @Input() budgetItems: BudgetItem[];
   @Output() delete: EventEmitter<BudgetItem> = new EventEmitter<BudgetItem>();
+  @Output() update: EventEmitter<UpdateEvent> = new EventEmitter<UpdateEvent>();
 
   constructor(public dialog: MatDialog) { }
 
@@ -29,9 +31,17 @@ export class BudgetItemListComponent implements OnInit {
     });
     dialogRef.afterClosed().subscribe(result => {
       if(result) {
-        this.budgetItems[this.budgetItems.indexOf(item)] = result;
+        this.update.emit({
+          old: item,
+          new: result
+        })
       }
     })
   }
 
+}
+
+export interface UpdateEvent{
+  old: BudgetItem;
+  new: BudgetItem;
 }
